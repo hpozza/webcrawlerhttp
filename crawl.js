2b330973bcd1b9e408877fa1ad040053bf1e5ee1
hpozza <henriquepza@gmail.com>
2023-08-22 20:48:05 -0300

    added normalizeUrl and tests

---
+++ crawl.js
@@ -0,0 +1,13 @@
+function normalizeURL(url) {
+    const urlObj = new URL(url);
+
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1);
+    }
+    return hostPath;
+}
+
+module.exports = {
+    normalizeURL
+}
---
+++ crawl.test.js
@@ -0,0 +1,16 @@
+const {normalizeURL} = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
+
+test('normalizeURL strip protocol', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL capitals', () => {
+    const input = 'http://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
